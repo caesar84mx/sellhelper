f2d92afa7294104c3e948317ebf18c71f18f0303
dymnov.dm@gmail.com <1vfrcbvec1>
2017-07-19 04:23:43 -0400

    1.1 - pom, properties and config fixes

--- src/main/java/com/caesar_84/sellhelper/SellhelperApplication.java
+++ src/main/java/com/caesar_84/sellhelper/SellhelperApplication.java
@@ -1,9 +1,13 @@
 package com.caesar_84.sellhelper;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class SellhelperApplication {
 
 	public static void main(String[] args) {
