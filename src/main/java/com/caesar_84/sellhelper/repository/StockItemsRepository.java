fdb736434afd3342baef7dbb701e07891fe634ea
dymnov.dm@gmail.com <1vfrcbvec1>
2017-08-04 00:19:51 -0400

    3.3 - service layer for stock implementation.

--- src/main/java/com/caesar_84/sellhelper/repository/StockItemsRepository.java
+++ src/main/java/com/caesar_84/sellhelper/repository/StockItemsRepository.java
@@ -2,6 +2,22 @@ package com.caesar_84.sellhelper.repository;
 
 import com.caesar_84.sellhelper.domain.StockItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface StockItemsRepository extends JpaRepository<StockItem, Integer> {
+    @Query("SELECT i FROM StockItem i WHERE i.id=:item_id AND i.user.id=:user_id")
+    StockItem getByUserId(@Param("item_id") int id, @Param("user_id") int userId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM StockItem i WHERE i.id=:item_id AND i.user.id=:user_id")
+    int deleteByUserId(@Param("item_id") int id, @Param("user_id") int userId);
+
+    @Query("SELECT i FROM StockItem i WHERE i.user.id=:user_id")
+    List<StockItem> getAllByUserId(@Param("user_id") int userId);
 }
--- src/main/java/com/caesar_84/sellhelper/service/stock/StockServiceImpl.java
+++ src/main/java/com/caesar_84/sellhelper/service/stock/StockServiceImpl.java
@@ -4,6 +4,7 @@ import com.caesar_84.sellhelper.domain.Good;
 import com.caesar_84.sellhelper.domain.StockItem;
 import com.caesar_84.sellhelper.repository.GoodsRepository;
 import com.caesar_84.sellhelper.repository.StockItemsRepository;
+import com.caesar_84.sellhelper.util.CheckUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,31 +28,37 @@ public class StockServiceImpl implements StockService {
     @Transactional
     @Override
     public StockItem save(Good good, int quantity, int userId) {
-        return null;
+        CheckUtil.checkUserIdConsistent(good.getUser(), userId);
+
+        Good savedGood = goodsRepository.save(good);
+
+        return stockItemsRepository.save(new StockItem(savedGood, quantity, savedGood.getUser()));
     }
 
     @Secured("ROLE_USER")
     @Transactional
     @Override
     public StockItem update(StockItem item, int userId) {
-        return null;
+        CheckUtil.checkUserIdConsistent(item.getUser(), userId);
+        goodsRepository.save(item.getGood());
+
+        return stockItemsRepository.save(item);
     }
 
-    @Transactional
     @Override
     public StockItem get(int id, int userId) {
-        return null;
+        return stockItemsRepository.getByUserId(id, userId);
     }
 
+    @Secured("ROLE_USER")
     @Transactional
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return stockItemsRepository.deleteByUserId(id, userId) != 0;
     }
 
-    @Transactional
     @Override
     public List<StockItem> getAll(int userId) {
-        return null;
+        return stockItemsRepository.getAllByUserId(userId);
     }
 }
