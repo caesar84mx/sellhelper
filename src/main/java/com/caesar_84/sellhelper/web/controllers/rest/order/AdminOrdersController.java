6a39d117e0a22a8be9e846e5ac3d03d9ab87abef
dymnov.dm@gmail.com <1vfrcbvec1>
2017-08-14 20:23:02 -0400

    4.7.1. - the rest of admin controller creation.

--- src/main/java/com/caesar_84/sellhelper/web/controllers/rest/order/AdminOrdersController.java
+++ src/main/java/com/caesar_84/sellhelper/web/controllers/rest/order/AdminOrdersController.java
@@ -1,20 +1,29 @@
 package com.caesar_84.sellhelper.web.controllers.rest.order;
 
 
+import com.caesar_84.sellhelper.LoggedUser;
 import com.caesar_84.sellhelper.domain.Order;
+import com.caesar_84.sellhelper.domain.auxclasses.OrderStatus;
 import com.caesar_84.sellhelper.service.CommonCrudService;
+import com.caesar_84.sellhelper.service.order.OrderService;
 import com.caesar_84.sellhelper.web.controllers.AbstractCommonCrudController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = AdminOrdersController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdminOrdersController extends AbstractCommonCrudController<Order> {
     static final String REST_URL = AbstractCommonCrudController.BASE_URL + "/admin/{user}/orders";
+
+    @Autowired
+    private OrderService service;
 
     @Autowired
     public AdminOrdersController(CommonCrudService<Order> commonCrudService) {
@@ -32,5 +41,32 @@ public class AdminOrdersController extends AbstractCommonCrudController<Order> {
     @GetMapping
     public List<Order> getAll(@PathVariable("user") int userId) {
         return super.getAll(userId);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/filtered/by-status")
+    public List<Order> getByStatus(@RequestParam(value = "status") OrderStatus status,
+                                   @PathVariable("user") int userId) {
+        return service.getByStatus(status, userId);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/filtered/by-client/{id}")
+    public List<Order> getByClient(@PathVariable("id") int clientId,
+                                   @PathVariable("user") int userId) {
+        return service.getByClient(clientId, userId);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/filtered/by-date-and-time")
+    public List<Order> getBetween(@RequestParam(value = "startDate", required = false) LocalDate startDate,
+                                  @RequestParam(value = "startTime", required = false) LocalTime startTime,
+                                  @RequestParam(value = "endDate", required = false) LocalDate endDate,
+                                  @RequestParam(value = "endTime", required = false) LocalTime endTime,
+                                  @PathVariable("user") int userId) {
+        LocalDateTime start = LocalDateTime.of(startDate, startTime);
+        LocalDateTime end = LocalDateTime.of(endDate, endTime);
+
+        return service.getBetween(start, end, userId);
     }
 }
--- src/main/java/com/caesar_84/sellhelper/web/controllers/rest/user/AdminUserController.java
+++ src/main/java/com/caesar_84/sellhelper/web/controllers/rest/user/AdminUserController.java
@@ -1,11 +1,16 @@
 package com.caesar_84.sellhelper.web.controllers.rest.user;
 
+import com.caesar_84.sellhelper.domain.User;
 import com.caesar_84.sellhelper.repository.UserRepository;
 import com.caesar_84.sellhelper.web.controllers.AbstractCommonCrudController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = AdminUserController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -15,5 +20,25 @@ public class AdminUserController {
     @Autowired
     private UserRepository repository;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping(value = "/{user}")
+    public User get(@PathVariable("user") int id) {
+        return repository.findOne(id);
+    }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping
+    public List<User> getAll() {
+        return repository.findAll();
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping(value = "/{user}")
+    public ResponseEntity enable(@PathVariable("user") int userId,
+                                 @RequestParam(value = "enable") boolean enable) {
+
+        return repository.setEnabled(userId, enable) != 0 ?
+                ResponseEntity.status(HttpStatus.OK).header("User disabled").build() :
+                ResponseEntity.status(HttpStatus.NOT_MODIFIED).header("Failed disabling user").build();
+    }
 }
